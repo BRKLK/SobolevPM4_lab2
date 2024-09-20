2b4b581ff7974cf911a7d94330b3ab48b69e42f1
BRKLK <burkalyek@gmail.com>
2024-09-20 22:16:50 +0200

    in

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    short a;
+    cout << "Input a value: "; cin >> a;
+    cout << "The cube volume is " << pow(a, 3);
+    return 1;
+}
